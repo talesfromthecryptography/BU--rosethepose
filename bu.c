0d067cef374dc3552412b789f0a4a6e7eb49eb34
Rose Ramirez <44708382+rosethepose@users.noreply.github.com>
2019-09-17 19:11:24 -0700

    :(

--- bu.c
+++ bu.c
@@ -1,58 +1,52 @@
 #include <string.h> // for memset, etc.
 #include <stdio.h>  // for printf
-
 #include "bu.h"
-
 // NOTE: In this code "word" always refers to a uint32_t
-
 // Copy dest = src
 void bu_cpy(bigunsigned *dest, bigunsigned *src) {
   uint16_t cnt = src->used;
-  dest->used = cnt;
-  dest->base = 0;
-
-  // reset upper 0s in dest
-  memset(dest->digit, 0, sizeof(uint32_t)*BU_DIGITS-cnt);
-
-  uint8_t i_dest = 0; // TODO: This is wrong. Fix it.
-  uint8_t i_src = src->base;
-
-  while (cnt-- > 0) {
-    dest->digit[i_dest--] = src->digit[i_src--];
-  }
-}
-
+    dest->used = cnt;
+    dest->base = 0;
+    // reset upper 0s in dest
+    memset(dest->digit + cnt, 0, sizeof(uint32_t)*(BU_DIGITS-cnt));
+    uint8_t i_dest = dest->base;
+    uint8_t i_src = src->base;
+    while (cnt-- > 0) {
+      dest->digit[i_dest++] = src->digit[i_src++];
+    }
+  }                                                                             
 // Set to 0
 void bu_clear(bigunsigned *a_ptr) {
   memset(a_ptr->digit, 0, sizeof(uint32_t)*BU_DIGITS);
   a_ptr->used = 0;
   a_ptr->base = 0;
 }
-
 // Shift in place a bigunsigned by cnt bits to the left
 // Example: beef shifted by 4 results in beef0
 void bu_shl_ip(bigunsigned* a_ptr, uint16_t cnt) {
-  uint16_t wrds = cnt >> 5; // # of whole words to shift
-  uint16_t bits = cnt &0x1f;// number of bits in a word to shift
+  //uint16_t wrds = cnt >> 5; // # of whole words to shift
+  //uint16_t bits = cnt &0x1f;// number of bits in a word to shift
 
-  uint32_t mask = 0xffffffff << bits;
+  //uint32_t mask = 0xffffffff << bits;
+
+  base = (base -1) % 256;
+  digit[base] = 0;
 
   // You implement. Avoid memory copying as much as possible.
 }
-
 // Produce a = b + c
 void bu_add(bigunsigned *a_ptr, bigunsigned *b_ptr, bigunsigned *c_ptr) {
   uint8_t carry = 0;
   uint64_t nxt;
   uint16_t cnt = 0;
-  uint16_t min_used = b_ptr->used <= c_ptr->used 
+  uint16_t min_used = b_ptr->used <= c_ptr->used
                       ? b_ptr->used : c_ptr->used;
   uint8_t  b_dig = b_ptr->base;
   uint8_t  c_dig = c_ptr->base;
   uint8_t  a_dig = 0;
 
   while (cnt < min_used) {
-    nxt = ((uint64_t)b_ptr->digit[b_dig++]) 
+    nxt = ((uint64_t)b_ptr->digit[b_dig++])
           + (uint64_t)(c_ptr->digit[c_dig++]) + carry;
     carry = 0 != (nxt&0x100000000);
     a_ptr->digit[a_dig++] = (uint32_t)nxt;
@@ -69,7 +63,7 @@ void bu_add(bigunsigned *a_ptr, bigunsigned *b_ptr, bigunsigned *c_ptr) {
   while (cnt < b_ptr->used) {
     a_ptr->digit[a_dig++] = b_ptr->digit[b_dig++];
     cnt++;
-  }  
+  }
 
   while (cnt < c_ptr->used && carry) {
     nxt = ((uint64_t)c_ptr->digit[c_dig++]) + carry;
@@ -92,7 +86,6 @@ void bu_add(bigunsigned *a_ptr, bigunsigned *b_ptr, bigunsigned *c_ptr) {
   a_ptr->base = 0;
   a_ptr->used = cnt;
 }
-
 // return the length in bits (should always be less or equal to 32*a->used)
 uint16_t bu_len(bigunsigned *a_ptr) {
   uint16_t res = a_ptr->used<<5;
@@ -105,29 +98,31 @@ uint16_t bu_len(bigunsigned *a_ptr) {
   }
   return res;
 }
-
 // Read from a string of hex digits
-//
 // TODO: This is wrong. See the test main.c
 //       Modify to resolve 'endian' conflict.
 //       Also modify to permit strings to include whitespace
 //        that will be ignored. For example, "DEAD BEEF" should
 //        be legal input resulting in the value 0xDEADBEEF.
-
 void bu_readhex(bigunsigned * a_ptr, char *s) {
   bu_clear(a_ptr);
 
   unsigned pos = 0;
-  char *s_ptr = s;
-  while (*s_ptr && pos < BU_MAX_HEX) {
-    a_ptr->digit[pos>>3] |= (((uint32_t)hex2bin(*s_ptr)) << ((pos & 0x7)<<2));
-    pos++;
-    s_ptr++;
+
+    char *s_ptr = s + strlen(s) - 1; //start from the back
+    while (s_ptr >= s && pos < BU_MAX_HEX) {
+      if(s_ptr[pos] != ' ') { //use iswhitespace method???
+        a_ptr->digit[pos>>3] |= (((uint32_t)hex2bin(*s_ptr)) << ((pos & 0x7)<<2));
+        pos++;
+      }
+      s_ptr--;
+    //a_ptr->digit[pos>>3] |= (((uint32_t)hex2bin(*s_ptr)) << ((pos & 0x7)<<2));
+    //pos++;
+    //s_ptr++;
+    printf("*s_ptr: %x\npos: %d\n",a_ptr->digit[0], pos);
   }
   a_ptr->used = (pos>>3) + ((pos&0x7)!=0);
 }
-
-// 
 void bu_dbg_printf(bigunsigned *a_ptr) {
   printf("Used %x\n", a_ptr->used);
   printf("Base %x\n", a_ptr->base);
